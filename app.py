e0a2d9447671bc16e5d30e336c4050437a892669
Darsh-14 <bipinkumar620013@gmail.com>
2025-04-06 20:08:12 +0530

    Remove Flask run for Cloud Run deployment

--- app.py
+++ app.py
@@ -343,8 +343,8 @@ def internal_error(e):
 
 import os
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))
-    app.run(host='0.0.0.0', port=port)
+# if __name__ == "__main__":
+#     port = int(os.environ.get("PORT", 8080))
+#     app.run(host='0.0.0.0', port=port)
 
 
